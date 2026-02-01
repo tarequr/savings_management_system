4baf8d08b9b14dd17706e6005c8b4877fb21c7a0
tarequr <tarequrr8@gmail.com>
2026-02-01 23:32:39 +0600

    chore: seeder modified

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,7 +15,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // Create Admin
         User::factory()->create([
             'name' => 'Admin User',
             'email' => 'admin@app.com',
@@ -24,10 +23,54 @@ class DatabaseSeeder extends Seeder
             'phone' => '01234567890',
         ]);
 
-        // Create Members
-        User::factory(10)->create([
-            'role' => 'member',
-            'password' => bcrypt('password'),
-        ]);
+        $members = [
+            [
+                'name' => 'Masum Billah',
+                'email' => 'masum@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567891',
+            ],
+            [
+                'name' => 'Taiyebur Rahman Tipu',
+                'email' => 'tipu@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567892',
+            ],
+            [
+                'name' => 'Tarequr Rahman Sabbir',
+                'email' => 'sabbir@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567893',
+            ],
+            [
+                'name' => 'Sohag Sarder',
+                'email' => 'sohag@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567894',
+            ],
+            [
+                'name' => 'Humaun Kabir',
+                'email' => 'kabir@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567895',
+            ],
+            [
+                'name' => 'Tanjim Hasan Radin',
+                'email' => 'radin@app.com',
+                'password' => bcrypt('password'),
+                'role' => 'member',
+                'phone' => '01234567896',
+            ]
+        ];
+
+        foreach ($members as $member) {
+            User::create($member);
+        }
+
     }
 }
